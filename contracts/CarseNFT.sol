e9fba87e12bc13c301a5ac57df6d717697d82b8e
Alessandro Solbiati <lessandro@fb.com>
2022-04-17 14:37:53 +0100

    [contract] CarseNTF.sol

---
+++ contracts/CarseNFT.sol
@@ -0,0 +1,48 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract CarseNFT is ERC721 {
+	using Counters for Counters.Counter;
+	Counters.Counter private _tokenIDs;
+	mapping(string => uint8) hash_already_taken;
+	mapping(uint256 => string) tokenID_to_hash;
+
+
+	constructor() public ERC721("CarseNFT", "carse") {}
+
+	function mint(address recipient, string memory hash)
+		public
+		returns (uint256)
+
+	{
+		require(hashes[hash] != 1);
+		hashes[hash] = 1;
+		_tokenIDs.increment();
+		uint256 newItemID = _tokenIDs.current();
+
+		_mint(recipient, newItemID);
+
+		return newItemID;
+	}
+
+	/**
+	* [0, allTokens()] is an array of all the minted tokens IDs 
+	*/
+	function allTokens() public return (uint8){
+		return _tokenIDs.current();
+	}
+
+	function tokenURI(uint256 tokenID) public view virtual override returns (string memory) {
+		require(_exists(tokenID), "ERC721Metadata: URI query for nonexistent token");
+
+		string memory hash = tokenID_to_hash[tokenID];
+    string memory baseURI = _baseURI();
+    return string(abi.encodePacked(baseURI, hash));
+   }
+
+	function _baseURI() internal view virtual returns (string memory) {
+			return "ipfs://";
+	}
+}
